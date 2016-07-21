aa19086ca55291fa2f5ceaa3f7363e778ed8a126
Sefa Eyeoglu <sefa6727@gmail.com>
2016-07-21 22:02:53 +0200

    Update to Sprummlbot 0.4.8

--- src/net/scrumplex/waitingfor/Main.java
+++ src/net/scrumplex/waitingfor/Main.java
@@ -1,5 +1,6 @@
 package net.scrumplex.waitingfor;
 
+import com.github.theholywaffle.teamspeak3.TS3ApiAsync;
 import com.github.theholywaffle.teamspeak3.api.CommandFuture;
 import com.github.theholywaffle.teamspeak3.api.event.ClientJoinEvent;
 import com.github.theholywaffle.teamspeak3.api.event.ClientLeaveEvent;
@@ -11,50 +12,50 @@ import net.scrumplex.sprummlbot.plugins.events.ClientJoinEventHandler;
 import net.scrumplex.sprummlbot.plugins.events.ClientQuitEventHandler;
 import net.scrumplex.sprummlbot.wrapper.ChatCommand;
 import net.scrumplex.sprummlbot.wrapper.CommandResponse;
-
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.TimeUnit;
 
 public class Main extends SprummlbotPlugin {
 
     private Map<Integer, String> watcherList = new HashMap<>();
+    private TS3ApiAsync api;
 
     @Override
     public void onEnable() {
+        api = getSprummlbot().getDefaultAPI();
         registerCommands();
         registerEvents();
     }
 
     private void registerCommands() {
-        ChatCommand cmdWaitingFor = getCommandManager().registerCommand("waitingfor", "!waitingfor [name/uid]");
+        ChatCommand cmdWaitingFor = getSprummlbot().getCommandManager().registerCommand("waitingfor", "!waitingfor [name/uid]");
         cmdWaitingFor.setCommandHandler(this, new CommandHandler() {
             @Override
             public CommandResponse handleCommand(final ClientInfo c, final String[] args) throws Exception {
                 if (args.length > 0) {
                     final String arg0 = args[0];
                     if (arg0.endsWith("=") && arg0.length() == 28) {
-                        getAPI().getDatabaseClientByUId(arg0).onSuccess(new CommandFuture.SuccessListener<DatabaseClientInfo>() {
+                        api.getDatabaseClientByUId(arg0).onSuccess(new CommandFuture.SuccessListener<DatabaseClientInfo>() {
                             @Override
                             public void handleSuccess(DatabaseClientInfo databaseClientInfo) {
                                 watcherList.put(c.getId(), databaseClientInfo.getUniqueIdentifier());
-                                getAPI().sendPrivateMessage(c.getId(), databaseClientInfo.getNickname() + " was added to the waiting list!");
+                                api.sendPrivateMessage(c.getId(), databaseClientInfo.getNickname() + " was added to the waiting list!");
                             }
                         });
                         return CommandResponse.SUCCESS;
                     } else {
-                        getAPI().getDatabaseClientsByName(arg0).onSuccess(new CommandFuture.SuccessListener<List<DatabaseClientInfo>>() {
+                        api.getDatabaseClientsByName(arg0).onSuccess(new CommandFuture.SuccessListener<List<DatabaseClientInfo>>() {
                             @Override
                             public void handleSuccess(List<DatabaseClientInfo> databaseClientInfos) {
                                 if (databaseClientInfos.size() == 0) {
-                                    getAPI().sendPrivateMessage(c.getId(), "Nothing found for " + arg0 + "!");
+                                    api.sendPrivateMessage(c.getId(), "Nothing found for " + arg0 + "!");
                                 } else if (databaseClientInfos.size() == 1) {
                                     DatabaseClientInfo databaseClientInfo = databaseClientInfos.get(0);
                                     watcherList.put(c.getId(), databaseClientInfo.getUniqueIdentifier());
-                                    getAPI().sendPrivateMessage(c.getId(), databaseClientInfo.getNickname() + " was added to the waiting list!");
+                                    api.sendPrivateMessage(c.getId(), databaseClientInfo.getNickname() + " was added to the waiting list!");
                                 } else {
-                                    getAPI().sendPrivateMessage(c.getId(), "There were too many results in the database! " + arg0 + " was not added to watching list!");
+                                    api.sendPrivateMessage(c.getId(), "There were too many results in the database! " + arg0 + " was not added to watching list!");
                                 }
                             }
                         });
@@ -73,7 +74,7 @@ public class Main extends SprummlbotPlugin {
                 if (watcherList.containsValue(e.getUniqueClientIdentifier())) {
                     for (Integer clientId : watcherList.keySet()) {
                         if (watcherList.get(clientId).equals(e.getUniqueClientIdentifier())) {
-                            getAPI().pokeClient(clientId, e.getClientNickname() + " joined the server!");
+                            api.pokeClient(clientId, e.getClientNickname() + " joined the server!");
                             watcherList.remove(clientId);
                         }
                     }
